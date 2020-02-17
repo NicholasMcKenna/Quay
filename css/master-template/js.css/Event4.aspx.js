f08f8e6ad791a44eba9e5d8cf4e196bba9b837a2
AP <andrew.pielorz@thelowry.com>
2020-02-17 13:20:56 +0000

    Pagination

--- css/master-template/js.css/Event4.aspx.js
+++ css/master-template/js.css/Event4.aspx.js
@@ -12,11 +12,4 @@ $(document).ready(function() {
     }
 
 
-
-
-
-    $.getScript("../../../js/pagination-js/paginator.js", function() {
-        console.log("Pagination Script loaded.");
-    });
-    
 });
---
+++ css/master-template/js.css/RecommendedShows.aspx.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+
+    $.getScript("../../../../js/pagination-js/paginator.js", function() {
+        console.log("Pagination Script loaded.");
+    });
+
+});
