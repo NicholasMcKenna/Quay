689001b7d0f34b4fa75b1a7a15136ddc42e07132
NicholasM <nicholas.a.mckenna@gmail.com>
2020-02-18 11:47:30 +0000

    test promo box

--- css/lowry.css/Event4.aspx.js
+++ css/lowry.css/Event4.aspx.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
     if ($('.hallMap').length > 0) {
         $('#hallMap').after($('.promoBox'));
     } else if ($('.selected').length > 0) {
+        /*
         if ($(window).width() >= 780) {
             $('.seatingPlanContainer').after('<div class="promo-container"></div>');
             $('.promo-container').after($('.promoBox'));
         } else {
             //$('#ticketsPane').prepend($('.promoBox'));
         }
+        */
     }
 });
