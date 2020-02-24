24cfce04a097844041cb666185bca1a7124c3aa0
NicholasM <nicholas.a.mckenna@gmail.com>
2020-02-24 13:46:45 +0000

    switch order

--- css/lowry.css/Checkout.aspx.js
+++ css/lowry.css/Checkout.aspx.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
     //$('.suppPaymentsSection').before($('#leftToPayContainer, #paymentSection'));
+    $('#leftToPayContainer').after($('.suppPaymentsSection'));
 });
