00476b1fba8efc7d13f12269f7cae910012fc708
NicholasM <nicholas.a.mckenna@gmail.com>
2020-02-26 11:09:09 +0000

    Add JS to place payment partner message

--- css/lowry.css/Checkout.aspx.js
+++ css/lowry.css/Checkout.aspx.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
     //$('.suppPaymentsSection').before($('#leftToPayContainer, #paymentSection'));
     $('#leftToPayContainer').after($('.suppPaymentsSection'));
+    $('#lblChoosePayment').after($('.ph_partner-text'));
 });
