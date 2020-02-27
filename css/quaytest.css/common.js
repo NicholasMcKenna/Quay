c9b052adfe634221e5526795236e003356583d3e
NicholasM <nicholas.a.mckenna@gmail.com>
2020-02-27 11:07:47 +0000

    changes to allow search bar to appear

--- css/quaytest.css/common.js
+++ css/quaytest.css/common.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    
     $('.flm_my_basket::after').css('content', 'Basket');
     $('.search-button').click(function() { $('#group_Header-fixed #PH_Search_Box').toggle(); });
 })
