0a92ad9226c907450c8ae58dd4823528897baced
NicholasM <nicholas.a.mckenna@gmail.com>
2020-02-27 11:29:05 +0000

    fix search box

--- css/quaytest.css/common.js
+++ css/quaytest.css/common.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
     $('.flm_my_basket::after').css('content', 'Basket');
-    $('.search-button').click(function() { 
+    $('.search-button').click(function(e) { 
         $('#group_Header-fixed #PH_Search_Box').toggle(); 
         $('#group_Header-fixed div[id^="PH_SearchPage"]').toggle();
+        e.stopPropagation();
     });
 })
 
+$(window).click(function() {
+    if ($('#group_Header-fixed #PH_Search_Box').is(":visible")) {
+        $('#group_Header-fixed #PH_Search_Box').hide(); 
+        $('#group_Header-fixed div[id^="PH_SearchPage"]').hide();
+    }
+});
