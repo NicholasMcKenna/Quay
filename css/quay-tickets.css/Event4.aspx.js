58631e5bf34e8d3d3bc83197fdd28e56f12e5299
NicholasM <nicholas.a.mckenna@gmail.com>
2020-04-06 11:10:40 +0100

    bug fix promo code

--- css/quay-tickets.css/Event4.aspx.js
+++ css/quay-tickets.css/Event4.aspx.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-    if ($('.hallMap').length > 0) {
-        $('#hallMap').after($('.promoBox'));
-    } else if ($('.selected').length > 0) {
-        if ($(window).width() <= 780) {
-            $('.seatingPlanContainer').after('<div class="promo-container"></div>');
-            $('.promo-container').after($('.promoBox'));
-        } else {
-            $('.eventDetails').after($('.promoBox'));
-        }
-    }
-
     if ($('.promoBox button[aria-label="Remove coupon Affliates"]').length > 0) {
         $('.promoBox').hide();
-    }
+    } else {
+        if ($('.hallMap').length > 0) {
+            $('#hallMap').after($('.promoBox'));
+        } else if ($('.selected').length > 0) {
+            if ($(window).width() <= 780) {
+                $('.seatingPlanContainer').after('<div class="promo-container"></div>');
+                $('.promo-container').after($('.promoBox'));
+            } else {
+                $('.eventDetails').after($('.promoBox'));
+            }
+        }
+    }   
 });
