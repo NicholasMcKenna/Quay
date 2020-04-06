30ddc38124a238101d58835f16c04528cf2a637c
NicholasM <nicholas.a.mckenna@gmail.com>
2020-04-06 13:16:49 +0100

    remove all traces of affliliates

--- css/lowry.css/Order.aspx.js
+++ css/lowry.css/Order.aspx.js
@@ -5,12 +5,15 @@ $(document).ready(function() {
     if (promo) {
         promo.hide();
     }
+
     var basketProp = $('.basketProperty.Details span.small_text_a:contains("Affliates")');
     if (basketProp) {
         basketProp.parent().hide();
     }
+
     var couponApplied = $('.couponApplied span:contains("Affliates")');
     if (couponApplied) {
         couponApplied.parent().hide();
     }
+
 });
--- css/lowry.css/common.js
+++ css/lowry.css/common.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
     
     $('.flm_my_basket::after').css('content', 'Basket');
-    $('.search-button').click(function() { $('#group_Header-fixed #PH_Search_Box').toggle(); });
+	$('.search-button').click(function() { $('#group_Header-fixed #PH_Search_Box').toggle(); });
+	
+    var basketProp = $('.ph_basketwidget .basketProperty.Details span.small_text_a:contains("Affliates")');
+    if (basketProp) {
+        basketProp.parent().parent().hide();
+    }
 })
 
 /*
