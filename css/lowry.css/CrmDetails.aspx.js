c57840a0db74952dee58ec3912feb7e3c983c682
AP <andrew.pielorz@thelowry.com>
2020-04-21 10:19:05 +0100

    various

---
+++ css/lowry.css/CrmDetails.aspx.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    /*
+    if ($('.promoBox button[aria-label="Remove coupon Affliates"]').length > 0) {
+        $('.promoBox').hide();
+    } else {
+        if ($('.hallMap').length > 0) {
+            $('#hallMap').after($('.promoBox'));
+        } else if ($('.selected').length > 0) {
+            if ($(window).width() <= 780 && $('#eventPage.seated').length > 0) {
+                $('.seatingPlanContainer:first').after('<div class="promo-container"></div>');
+                $('.promo-container').after($('.promoBox'));
+            } else if ($(window).width() <= 780 && $('#eventPage.non-seated').length > 0) {
+                $('.buttonsContainer').before($('.promoBox'));
+            } else {
+                //$('#ticketsPane').prepend($('.promoBox'));
+                $('.eventDetails:first').after($('.promoBox'));
+            }
+        }
+    }
+
+    $('#zoomContainer').on("mousewheel DOMMouseScroll", function (e) {
+        e.stopPropagation()
+    });
+
+
+
+.loginOrRegisterWizard.su_preferences .wizardStepDescription
+
+
+
+    */
+
+
+});
