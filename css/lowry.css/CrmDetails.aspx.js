497135be8499af44ca1016da5b95dca1b5364a96
AP <andrew.pielorz@thelowry.com>
2020-04-21 11:00:58 +0100

    Update CrmDetails.aspx.js

--- css/lowry.css/CrmDetails.aspx.js
+++ css/lowry.css/CrmDetails.aspx.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    /*
+/*
     if ($('.promoBox button[aria-label="Remove coupon Affliates"]').length > 0) {
         $('.promoBox').hide();
     } else {
@@ -22,13 +22,12 @@ $(document).ready(function () {
         e.stopPropagation()
     });
 
+*/
 
 
-.loginOrRegisterWizard.su_preferences .wizardStepDescription
+    if ($('.loginOrRegisterWizard.su_preferences .wizardStepDescription').length) {
 
-
-
-    */
-
+        $('.loginOrRegisterWizard.su_preferences .wizardStepDescription').html("The Lowry is committed to protecting your personal data. We will always contact you about any important changes to your booking. We also use our legitimate business interests to process your data in order to provide you with the best experience and most relevant information. If you wish for your data not to be used in this way you can ask us not to. If you would prefer NOT to receive information by a particular channel, please UNTICK the relevant box:");
+    }
 
 });
