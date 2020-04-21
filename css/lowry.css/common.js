0c19a2f26092589229ba21e58c4e84a92090070d
AP <andrew.pielorz@thelowry.com>
2020-04-21 12:21:15 +0100

    lowry

--- css/lowry.css/common.js
+++ css/lowry.css/common.js
@@ -27,10 +27,9 @@ $(document).ready(function() {
                 $('.loginOrRegisterWizard.su_preferences .wizardStepDescription').html("The Lowry is committed to protecting your personal data. We will always contact you about any important changes to your booking. We also use our legitimate business interests to process your data in order to provide you with the best experience and most relevant information. If you wish for your data not to be used in this way you can ask us not to. If you would prefer NOT to receive information by a particular channel, please UNTICK the relevant box:");
             
                 suPrefTargetCopyDiv.css("display", "block");
-            //clearInterval(suPrefsInt);
+                //clearInterval(suPrefsInt);
             }
         }
-
     }
 
 });
