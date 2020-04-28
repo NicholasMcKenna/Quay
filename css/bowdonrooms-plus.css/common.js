5b22dd0c4f0e0f2a13c79bc2fc6529eb3a06e883
AP <andrew.pielorz@thelowry.com>
2020-04-28 13:42:30 +0100

    Bowdon Rooms Plus

---
+++ css/bowdonrooms-plus.css/common.js
@@ -0,0 +1,18 @@
+
+/*
+$(document).ready(function() {
+    $('.flm_my_basket::after').css('content', 'Basket');
+    $('.search-button').click(function(e) { 
+        $('#group_Header-fixed #PH_Search_Box').toggle(); 
+        $('#group_Header-fixed div[id^="PH_SearchPage"]').toggle();
+        e.stopPropagation();
+    });
+})
+*/
+/*
+$(window).click(function() {
+    if ($('#group_Header-fixed #PH_Search_Box').is(":visible")) {
+        $('#group_Header-fixed #PH_Search_Box').hide(); 
+        $('#group_Header-fixed div[id^="PH_SearchPage"]').hide();
+    }
+});*/
