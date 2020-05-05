d61cfbc05de99c9a7882e4420f8e675c4b443f32
AP <andrew.pielorz@thelowry.com>
2020-05-05 11:55:57 +0100

    TOC2

--- css/theoldcourts-plus.css/common.js
+++ css/theoldcourts-plus.css/common.js
@@ -1,5 +1,5 @@
 
-/*
+
 $(document).ready(function() {
     $('.flm_my_basket::after').css('content', 'Basket');
     $('.search-button').click(function(e) { 
@@ -8,11 +8,12 @@ $(document).ready(function() {
         e.stopPropagation();
     });
 })
-*/
+
 /*
 $(window).click(function() {
     if ($('#group_Header-fixed #PH_Search_Box').is(":visible")) {
         $('#group_Header-fixed #PH_Search_Box').hide(); 
         $('#group_Header-fixed div[id^="PH_SearchPage"]').hide();
     }
-});*/
+});
+*/
