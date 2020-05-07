19f410f97f2bc248615804658c4afb376d03c2d0
NicholasM <nicholas.a.mckenna@gmail.com>
2020-05-07 08:25:55 +0100

    If ladyboys change row to table

--- css/quaytickets.css/common.js
+++ css/quaytickets.css/common.js
@@ -7,8 +7,14 @@ $(document).ready(function() {
         $('#group_Header-fixed div[id^="PH_SearchPage"]').toggle();
         e.stopPropagation();
     });
-})
 
+    if ($('#eventPage .infoSection .name:contains(The Lady Boys of Bangkok)')) {
+
+        $(document).on('basketUpdated.esro', function() {
+            $('.selectTickets .row .seat>.rowName').text('Table');
+        });
+    }
+});
 /*
 $(window).click(function() {
     if ($('#group_Header-fixed #PH_Search_Box').is(":visible")) {
