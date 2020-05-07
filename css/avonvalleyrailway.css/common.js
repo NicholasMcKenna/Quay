99a963c4e3695134f4f0927578716ac5b8ce4f88
AP <andrew.pielorz@thelowry.com>
2020-05-07 11:54:10 +0100

    sync

--- css/avonvalleyrailway.css/common.js
+++ css/avonvalleyrailway.css/common.js
@@ -1,9 +1,10 @@
+/*
 $(document).ready(function() {
     
     $('.flm_my_basket::after').css('content', 'Basket');
     $('.search-button').click(function() { $('#group_Header-fixed #PH_Search_Box').toggle(); });
 })
-
+*/
 /*
  * jQuery ShowMore plugin 0.0.1
  *
